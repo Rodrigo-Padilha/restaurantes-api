852bb9749e1743a19bf53ee45d12305e3ad2d7c8
Pichau <pichau@email.com>
2026-01-16 22:34:20 -0300

    API restaurantes em produção

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./firebase-key.json"); // seu arquivo do firebase
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+// rota teste
+app.get("/", (req, res) => {
+  res.send("API Restaurantes ativa 🚀");
+});
+
+// webhook da cakto
+app.post("/cakto-webhook", async (req, res) => {
+  try {
+    const payload = req.body;
+
+    console.log("JSON COMPLETO DA CAKTO:");
+    console.log(JSON.stringify(payload, null, 2));
+
+    const email = payload.data.customer.email;
+    const senha = payload.data.customer.docNumber;
+    const nome = payload.data.customer.name;
+
+    const user = await admin.auth().createUser({
+      email: email,
+      password: senha,
+      displayName: nome
+    });
+
+    console.log("USUÁRIO CRIADO NO FIREBASE:", user.uid);
+
+    res.status(200).json({ success: true });
+
+  } catch (err) {
+    console.error("ERRO AO CRIAR USUÁRIO:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ativo na porta 3000");
+});
