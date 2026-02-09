6d896bef50215b70db482905e3d4256a82f3c8a1
Pichau <pichau@email.com>
2026-02-08 22:13:18 -0300

    update webhook segurança

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
+const crypto = require("crypto");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
@@ -13,15 +14,41 @@ app.use(bodyParser.json());
 
 // rota teste
 app.get("/", (req, res) => {
-  res.send("API Restaurantes ativa 🚀");
+  res.send("API ativa");
 });
 
 // webhook da cakto
-app.post('/webhook', express.json(), (req, res) => {
-  console.log('HEADERS RECEBIDOS:', req.headers);
-  console.log('BODY RECEBIDO:', req.body);
+app.post("/cakto-webhook", async (req, res) => {
+  try {
 
-  res.sendStatus(200);
+    console.log("WEBHOOK RECEBIDO:");
+    console.log(JSON.stringify(req.body, null, 2));
+
+    const payload = req.body;
+
+    if (!payload || !payload.data || !payload.data.customer) {
+      console.log("payload fora do padrão");
+      return res.status(400).send("payload inválido");
+    }
+
+    const email = payload.data.customer.email;
+    const senha = payload.data.customer.docNumber;
+    const nome = payload.data.customer.name;
+
+    const user = await admin.auth().createUser({
+      email: email,
+      password: senha,
+      displayName: nome
+    });
+
+    console.log("USUÁRIO CRIADO:", user.uid);
+
+    res.status(200).json({ success: true });
+
+  } catch (err) {
+    console.error("ERRO:", err.message);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(3000, () => {
